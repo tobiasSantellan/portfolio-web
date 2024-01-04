32088f4bd42ee20a742542e909abbd3d77ea919a
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-01-04 18:22:15 -0300

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import HeroImage from "../assets/banner.svg";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import HeroImage from "../assets/profile-image.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div
-      name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
-    >
+    <div name="home" className="h-screen w-full bg-black">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+          <h2 className="text-3xl sm:text-7xl font-bold text-white mt-3">
+            I'm a <span className="text-green"> Frontend Developer</span>
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            My name is Tobias Santellan. Passionate about what I do and very
+            focused on continuing to develop my skills day by day. Always ready
+            for any situation in which I can contribute with my knowledge and
+            learn something new.
           </p>
 
           <div>
@@ -25,11 +23,11 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-green cursor-pointer"
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
-                <MdKeyboardArrowRight size={25} className="ml-1" />
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
           </div>
@@ -39,7 +37,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full hover:saturate-200"
+            className="rounded-1xl mx-auto w-3/4 opacity-2 me-6"
           />
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#00a98f",
+      },
+    },
   },
   plugins: [],
 };
